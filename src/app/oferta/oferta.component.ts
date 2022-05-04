6a44cdd052f0fede272ae6042eb57e71e71a9132
juholiver <juholiver3@gmail.com>
2022-05-04 11:14:31 -0300

    AdcBotaoPesquisaNoTempleteTopo

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
-import { Observable } from 'rxjs';
-import { interval } from 'rxjs';
-import { Observer } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-oferta',
@@ -12,7 +11,7 @@ import { Observer } from 'rxjs';
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
 
 
   public oferta: Oferta | undefined
@@ -29,32 +28,10 @@ export class OfertaComponent implements OnInit {
       this.oferta = oferta
       //console.log(this.oferta)
     })
+  }
 
-/*
-    this.route.params.subscribe(
-      (parametro: any) => {console.log(parametro) },
-      (erro: any) => console.log(erro),
-      () => console.log('processamento foi classificado com concluido!')
-      )
-      */
-
-     /* 
-    let tempo = interval(2000)
-
-    tempo.subscribe((intervalo: number) => {
-      console.log(intervalo);
-    })
-    */
-
-    //observable (observavel)
-    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-        observer.next(1)
-        observer.next(3)
-    })
-    //observable (observador)
-    meuObservableTeste.subscribe(
-      (resultado: number) => console.log(resultado + 10)
-    )
+  ngOnDestroy(): void {
+    
   }
 
 }
