53e1a7ae517d8a5215071433568f2c3ab35902e3
juholiver <juholiver3@gmail.com>
2022-05-04 12:02:23 -0300

    LogOfertaParteError

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -5,6 +5,7 @@ import { Oferta } from "./shared/oferta.model"
 import { URL_API } from './app.api'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators';
+import { retry } from 'rxjs/operators';
 
 
 
@@ -53,6 +54,7 @@ export class OfertasService {
 
     public pesquisaOfertas(termo: string): Observable<Oferta[]>{
         return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
+            .pipe(retry(10)) 
             .pipe(map((resposta: any) => resposta))
     }
 }
