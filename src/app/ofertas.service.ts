6dc5ed98637b2a5f4cb2d780816496517a463859
juholiver <juholiver3@gmail.com>
2022-05-04 11:39:16 -0300

    ObservableParaPesquisa

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core'
 import { Oferta } from "./shared/oferta.model"
 
 import { URL_API } from './app.api'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators';
 
 
 
@@ -47,5 +49,10 @@ export class OfertasService {
         .then((resposta: any) => {
             return resposta[0].descricao
         })
+    }
+
+    public pesquisaOfertas(termo: string): Observable<Oferta[]>{
+        return this.http.get(`${URL_API}/ofertas?descricao_oferta=${termo}`)
+            .pipe(map((resposta: any) => resposta))
     }
 }
