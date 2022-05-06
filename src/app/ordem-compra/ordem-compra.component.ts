0849c7a8b22f164158509754c784f27bf1ab532d
juholiver <juholiver3@gmail.com>
2022-05-06 16:27:56 -0300

    ValidandoFeedBack

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,12 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = ''
   public formaPagamento: string =''
 
+  //controles de validação dos campos
+  public enderecoValido: boolean | undefined
+  public numeroValido: boolean | undefined
+  public complementoValido: boolean | undefined
+  public formaPagamentoValido: boolean | undefined
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,19 +25,44 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string) : void{
     this.endereco = String (endereco)
-    console.log(this.endereco)
+    //console.log(this.endereco)
+    //se a string for maior que 3
+    if (this.endereco.length > 3){
+      this.enderecoValido = true 
+    } else {
+      this.enderecoValido = false
+    }
   }
   public atualizaNumero(numero: string) : void{
     this.numero = String (numero)
-    console.log(this.numero)
+    //console.log(this.numero)
+
+
+    if(this.numero.length > 0) {
+      this.numeroValido = true
+    } else {
+      this.numeroValido = false
+    }
   }
   public atualizaComplemento(complemento: string) : void{
     this.complemento = String (complemento)
-    console.log(this.complemento)
+    //console.log(this.complemento)
+
+    if(this.complemento.length > 0) {
+      this.complementoValido = true
+    }else{
+      this.complementoValido = false
+    }
   }
   public atualizaFormaPagamento(formaPagamento: string) : void{
     this.formaPagamento = String (formaPagamento)
     console.log(this.formaPagamento)
+
+    if(this.formaPagamento.length > 0 ) {
+      this.formaPagamentoValido = true
+    } else {
+      this.formaPagamentoValido = false
+    }
   } 
   
 
