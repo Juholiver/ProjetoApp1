a981d24368042663fde59c85ba64750e02e4d06a
juholiver <juholiver3@gmail.com>
2022-05-06 12:57:42 -0300

    TwoWayBinding

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string =''
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public atualizaEndereco(endereco: string) : void{
+    this.endereco = String (endereco)
+    console.log(this.endereco)
+  }
+  public atualizaNumero(numero: string) : void{
+    this.numero = String (numero)
+    console.log(this.numero)
+  }
+  public atualizaComplemento(complemento: string) : void{
+    this.complemento = String (complemento)
+    console.log(this.complemento)
+  }
+  public atualizaFormaPagamento(formaPagamento: string) : void{
+    this.formaPagamento = String (formaPagamento)
+    console.log(this.formaPagamento)
+  } 
+  
 
 }
