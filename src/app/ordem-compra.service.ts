008859338d36391e56acdea0a894b560b7a156d9
juholiver <juholiver3@gmail.com>
2022-05-07 11:36:58 -0300

    FormularioHttpPost

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import { Pedido } from "./shared/pedido.model"
+import { Observable, map } from "rxjs"
+
+import { URL_API } from "./app.api"
 
 @Injectable()
 export class OrdemCompraService {
 
     constructor(private http: HttpClient) {}
 
-    
-    public efetivarCompra(pedido: Pedido): void {
-        console.log(pedido)
+
+    public efetivarCompra(pedido: Pedido): Observable<any> {
+         return this.http.post(
+            "http://localhost:3000/pedidos",
+            pedido
+            ).pipe(map((response: any ) => {
+                console.log(response)
+            }))
     }
 }
