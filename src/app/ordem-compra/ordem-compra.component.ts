ca0ba51086b873559b0f64c2a135239a24d9c63f
juholiver <juholiver3@gmail.com>
2022-05-07 14:05:11 -0300

    FormulariongSubit

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service';
 import { Pedido } from '../shared/pedido.model';
 
@@ -15,5 +16,7 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit(): void {
     
   }
-
+  public confirmarCompra(formulario: NgForm)  {
+    console.log(formulario)
+  }
 }
