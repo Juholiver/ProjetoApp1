cf295aeaf3c173da73f050d7b0539cdaa5eb5ff5
juholiver <juholiver3@gmail.com>
2022-05-07 07:18:15 -0300

    ServiçosOrdensDeCompra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdemCompraService } from '../ordem-compra.service';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
 
@@ -30,10 +32,12 @@ export class OrdemCompraComponent implements OnInit {
   public formEstado: string = 'disabled'
 
 
-  constructor() { }
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit(): void {
+    this.ordemCompraService.efetivarCompra()
   }
+
 
   public atualizaEndereco(endereco: string) : void{
     this.endereco = String (endereco)
